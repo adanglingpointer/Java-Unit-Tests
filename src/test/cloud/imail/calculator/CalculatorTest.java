1c7f3bda9398514d865a9b4a84c8a152eda1a9ec
adanglingpointer <dave@imail.cloud>
2024-02-01 21:26:08 -0500

    more unit tests

--- src/test/cloud/imail/calculator/CalculatorTest.java
+++ src/test/cloud/imail/calculator/CalculatorTest.java
@@ -1,12 +1,22 @@
 package cloud.imail.calculator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    @Test
     public void canAddZeroPlusZero() {
         Calculator calc = new Calculator();
         int sum = calc.add(0, 0);
-        assertEquals()
+        assertEquals(0, sum, "was expecting sum of 0");
+    }
+
+    @Test
+    public void canAddOnePlusOne() {
+        Calculator calc = new Calculator();
+        int sum = calc.add(1, 1);
+        assertEquals(2, sum);
     }
 }
