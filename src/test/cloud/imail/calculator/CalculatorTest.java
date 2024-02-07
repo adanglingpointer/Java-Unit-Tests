a9668becbe315647315626ddfc2a576c0fee4e8c
adanglingpointer <dave@imail.cloud>
2024-02-06 20:09:24 -0500

    more unit testing

--- src/test/cloud/imail/calculator/CalculatorTest.java
+++ src/test/cloud/imail/calculator/CalculatorTest.java
@@ -1,14 +1,22 @@
 package cloud.imail.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
+    }
+
     @Test
     public void canAddZeroPlusZero() {
-        Calculator calc = new Calculator();
         int sum = calc.add(0, 0);
         assertEquals(0, sum, "was expecting sum of 0");
     }
@@ -18,5 +26,26 @@ public class CalculatorTest {
         Calculator calc = new Calculator();
         int sum = calc.add(1, 1);
         assertEquals(2, sum);
+    }
+
+    @Test()
+    @Disabled
+    // @Ignore - for older than JUnit v5
+    public void canAddMaxIntPlusOne() {
+        int sum = calc.add(Integer.MAX_VALUE, 1);
+        System.out.println(sum);
+        assertEquals(Integer.MAX_VALUE + 1L, sum);
+    }
+
+    @Test
+    public void annuityExample() {
+        String answer = calc.calcAnnuity("22000", 7, ".06", 1);
+        assertEquals("$184,664.43", answer);
+    }
+
+    @Test
+    public void annuityPractice2() {
+        String answer = calc.calcAnnuity("1200", 10, ".08", 4);
+        assertEquals("$72,482.38", answer);
     }
 }
